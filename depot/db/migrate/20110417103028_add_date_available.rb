b6364e2c8d92af40205f15e634a8738f5d597c6f
daicham <daicham@gmail.com>
2011-04-17 19:43:14 +0900

    P.65(P.538) script/generate migration で作成されたファイルにカラム追加の記述を追記

--- depot/db/migrate/20110417103028_add_date_available.rb
+++ depot/db/migrate/20110417103028_add_date_available.rb
@@ -1,7 +1,9 @@
 class AddDateAvailable < ActiveRecord::Migration
   def self.up
+    add_column(:products, :date_available, :datetime, :null => false)
   end
 
   def self.down
+    remove_column(:products, :date_available)
   end
 end
