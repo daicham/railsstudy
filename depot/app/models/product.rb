657773e3d39adaca0c21c74524602ee2063dab4a
daicham <daicham@gmail.com>
2011-04-17 20:55:51 +0900

    P.69 URLフォーマットのチェックを追加

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ActiveRecord::Base
   validates_presence_of :title, :description, :image_url
   validates_numericality_of :price
   validates_uniqueness_of :title
-
+  validates_format_of :image_url,
+                      :with => %r{^http:.+\.(gif|jpg|png)$}i,
+                      :message => "は GIF、JPG、PNG画像のURLでなければなりません"
   protected
   def validate
     errors.add(:price, "は0より大きくなければなりません") unless price.nil? || price > 0.0
