9388d26cdb802d80930b0d6c27fa3023fec9f618
daicham <daicham@gmail.com>
2011-04-17 20:38:30 +0900

    P.68 0以上かどうかチェックし、独自のエラーメッセージを出力するようにした。

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   validates_presence_of :title, :description, :image_url
   validates_numericality_of :price
+
+  protected
+  def validate
+    errors.add(:price, "は0より大きくなければなりません") unless price.nil? || price > 0.0
+  end
 end
