471914406b11cd6b9a7ca601990189c9c284db9a
daicham <daicham@gmail.com>
2011-04-18 22:08:04 +0900

    P.75 Store画面にカタログを表示されるように変更

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
+    @products = Product.salable_items
   end
 
 end
--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -5,6 +5,13 @@ class Product < ActiveRecord::Base
   validates_format_of :image_url,
                       :with => %r{^http:.+\.(gif|jpg|png)$}i,
                       :message => "は GIF、JPG、PNG画像のURLでなければなりません"
+
+  def self.salable_items
+    find(:all,
+         :conditions => "date_available <= now()",
+         :order => "date_available desc")
+  end
+
   protected
   def validate
     errors.add(:price, "は0より大きくなければなりません") unless price.nil? || price > 0.0
